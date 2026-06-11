940d8d128f60f2cd142a5a2e40ae9eb250bbe72c
JHertz5 <jukka.hertzog@live.co.uk>
2026-06-11 20:16:59 +0100

    #14 Added RAM types.

--- src/common/hdl/lil_cpu_pkg.vhd
+++ src/common/hdl/lil_cpu_pkg.vhd
@@ -5,6 +5,7 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
+  use ieee.math_real.all;
 
 package lil_cpu_pkg is
 
@@ -14,12 +15,20 @@ package lil_cpu_pkg is
   subtype  t_bus_data  is std_logic_vector(c_bus_width - 1 downto 0);
   subtype  t_addr is unsigned(c_addr_width - 1 downto 0);
 
+  constant c_ram_depth : natural := c_addr_width ** 2;
+  type t_ram is array(c_ram_depth - 1 downto 0) of t_bus_data;
+
   -- Implement a bus transceiver. When not enabled, isolate the output from the input data, putting the output to
   -- high-impedance. When enabled, output the input data.
   function buffer_output_to_bus (
     i_en : std_logic;
     i_data : std_logic_vector
   ) return std_logic_vector;
+
+  -- Calculate ceiling(log2(x)). Useful for bit widths.
+  function clog2 (
+    i_arg : positive
+  ) return natural;
 
 end package;
 
@@ -38,5 +47,15 @@ package body lil_cpu_pkg is
     return v_result;
 
   end function;
+
+  function clog2 (
+    i_arg : positive
+  ) return natural is
+  begin
+
+    return integer(ceil(log2(real(i_arg))));
+
+  end function;
+
 
 end package body;
