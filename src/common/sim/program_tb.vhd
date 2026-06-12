3dceabb1d32b30dcf24ebaeeef15b5bdda2e5d4f
JHertz5 <jukka.hertzog@live.co.uk>
2026-06-12 22:29:00 +0100

    #54 Moved counter to be common module.

--- src/common/sim/program_tb.vhd
+++ src/common/sim/program_tb.vhd
@@ -13,19 +13,19 @@ library lil_cpu_lib;
   use lil_cpu_lib.common_tb_pkg.all;
   use lil_cpu_lib.lil_cpu_pkg.all;
 
-entity program_counter_tb is
+entity counter_tb is
   generic (
     runner_cfg : runner_cfg_t := runner_cfg_default
   );
 end entity;
 
-architecture tb of program_counter_tb is
+architecture tb of counter_tb is
 
   -- DUT signals.
   signal dut_i_clk      : std_logic := '0';
   signal dut_i_reset    : std_logic;
   signal dut_i_count_en : std_logic;
-  signal dut_o_addr     : t_addr;
+  signal dut_o_count     : t_addr;
 
 begin
 
@@ -39,12 +39,12 @@ begin
   -- Instantiate the DUT.
   ----------------------------------------------------------------------------------------------------------------------
 
-  cmp_dut : entity lil_cpu_lib.program_counter(rtl)
+  cmp_dut : entity lil_cpu_lib.counter(rtl)
     port map (
       i_clk      => dut_i_clk,
       i_reset    => dut_i_reset,
       i_count_en => dut_i_count_en,
-      o_addr     => dut_o_addr
+      o_count    => dut_o_count
     );
 
   ----------------------------------------------------------------------------------------------------------------------
@@ -53,9 +53,9 @@ begin
 
   proc_test_sequencer : process
 
-    variable v_exp_addr : t_addr;
+    variable v_exp_count : t_addr;
 
-    constant c_addr_test_name : string := "o_addr";
+    constant c_count_test_name : string := "o_count";
 
     -- Reset the register.
     procedure trigger_reset is
@@ -83,24 +83,24 @@ begin
       -- Test reset clears register to zero.
       if run("test_reset") then
         wait until rising_edge(dut_i_clk);
-        check_unsigned(c_addr_test_name, dut_o_addr, (t_addr'range => 'U'));
-        v_exp_addr     := x"0";
+        check_unsigned(c_count_test_name, dut_o_count, (dut_o_count'range => 'U'));
+        v_exp_count     := x"0";
         trigger_reset;
-        check_unsigned(c_addr_test_name, dut_o_addr, v_exp_addr);
+        check_unsigned(c_count_test_name, dut_o_count, v_exp_count);
         dut_i_count_en <= '1';
         wait until rising_edge(dut_i_clk);
         trigger_reset;
-        check_unsigned(c_addr_test_name, dut_o_addr, v_exp_addr);
+        check_unsigned(c_count_test_name, dut_o_count, v_exp_count);
       end if;
 
-      -- Test addr increments on rising edge when count is enabled.
+      -- Test count increments on rising edge when count is enabled.
       if run("test_count_en") then
         trigger_reset;
         dut_i_count_en <= '1';
         for lv_iteration in natural range 1 to 10 loop
           wait until rising_edge(dut_i_clk);
-          v_exp_addr := to_unsigned(lv_iteration, t_addr'length);
-          check_unsigned(c_addr_test_name, dut_o_addr, v_exp_addr);
+          v_exp_count := to_unsigned(lv_iteration, dut_o_count'length);
+          check_unsigned(c_count_test_name, dut_o_count, v_exp_count);
         end loop;
       end if;
 
@@ -108,15 +108,15 @@ begin
       if run("test_count_disabled_holds_count") then
         trigger_reset;
         dut_i_count_en <= '1';
-        v_exp_addr     := x"0";
-        check_unsigned(c_addr_test_name, dut_o_addr, v_exp_addr);
+        v_exp_count     := x"0";
+        check_unsigned(c_count_test_name, dut_o_count, v_exp_count);
         wait until rising_edge(dut_i_clk);
-        v_exp_addr     := x"1";
+        v_exp_count     := x"1";
         dut_i_count_en <= '0';
         -- Wait multiple cycles with count disabled.
         for lv_iteration in natural range 1 to 10 loop
           wait until rising_edge(dut_i_clk);
-          check_unsigned(c_addr_test_name, dut_o_addr, v_exp_addr);
+          check_unsigned(c_count_test_name, dut_o_count, v_exp_count);
         end loop;
       end if;
 
